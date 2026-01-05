80aa13575d55d9c434b52727c04a3d0195b1cccb
김현호 <154233014+Stephan1226@users.noreply.github.com>
2026-01-05 15:57:06 +0900

    [Silver IV] Title: 수 찾기, Time: 256 ms, Memory: 41180 KB -BaekjoonHub

--- node.js/백준/Silver/1920. 수 찾기/수 찾기.js
+++ node.js/백준/Silver/1920. 수 찾기/수 찾기.js
@@ -1,9 +1,12 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt"
 let input = require("fs").readFileSync(filePath).toString().trim().split("\n")
 
+const result = [];
 const hasSet = new Set(input[1].split(' '));
 const needCheck = input[3].split(' ');
 
 for (let inputIndexCounter = 0; inputIndexCounter < needCheck.length; inputIndexCounter++) {
-  console.log(hasSet.has(needCheck[inputIndexCounter])? 1: 0);
+  result.push(hasSet.has(needCheck[inputIndexCounter])? 1: 0);
 }
+
+console.log(result.join('\n'));
